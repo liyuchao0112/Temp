67d3555b23eca0fa538f24a0763245d0cb3528fa
Liyuchao <2038429915@qq.com>
2022-01-01 13:42:46 +0800

    modified:   Source/main.cpp 	deleted:    build/.cmake/api/v1/reply/index-2022-01-01T05-08-21-0410.json 	modified:   build/.ninja_deps 	modified:   build/.ninja_log 	modified:   build/CMakeFiles/Temp.dir/Source/main.cpp.obj 	modified:   build/Temp.exe 	modified:   build/Temp.pdb 	Head/ 	build/.cmake/api/v1/reply/index-2022-01-01T05-41-35-0641.json

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include "../Head/RedBlackTree.h"
 using namespace std;
 int main() {
 	int n;
 	cin>>n;
-	vector<int> a(n);
-	for(int i=0;i<n;i++)
-		cin>>a[i];
-	sort(a.begin(),a.end(),[](int x,int y) -> bool {
-		return x<y;
-	});
-	for_each(a.begin(),a.end(),[](int x) -> void {
-		cout<<x<<' ';
-	});
+	bst t;
+	while(n--) {
+		int x;
+		cin>>x;
+		t.insert(x);
+	}
+	t.inorder();
 	return 0;
 }
